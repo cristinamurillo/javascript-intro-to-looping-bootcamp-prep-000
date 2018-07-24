d9c312c695d58c9cb93e36bee29a0dbf5d8b96ac
Cristina Murillo <cmurill1@jhu.edu>
2018-07-24 20:09:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,6 +2,7 @@ function forLoop(array){
   for(var i=0; i<25;i++){
     if(i===1){
       [...array, "I am 1 strange loop."]
+      return array
     } else {
       [...array, `I am ${i} strange loops.`]
     }
